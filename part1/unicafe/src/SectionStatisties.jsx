c62f5beae7d563f37ecb85a633e99c35bea0a445
artorias <agustincastro,irb@gmail.com>
2023-04-16 19:33:51 -0300

    1.7: unicafe, paso 2

--- part1/unicafe/src/SectionStatisties.jsx
+++ part1/unicafe/src/SectionStatisties.jsx
@@ -1,11 +1,18 @@
 
 const SectionStatisties = ({good, neutral, bad}) => {
+    const all = good + neutral + bad;
+    let average = (good + bad *-1) / 3
+    let positive = good ? (good * 100) / all : 0;
+    
     return(
         <section>
             <h2>statisties</h2>
             <p>Good {good}</p>
             <p>Neutral {neutral}</p>
             <p>Bad {bad}</p>
+            <p>all {all}</p>
+            <p>average {average}</p>
+            <p>positive {positive} %</p>
         </section>
     )
 }
