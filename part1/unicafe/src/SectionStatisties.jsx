071293d6e540d17914323b72aba5dcec34758047
artorias <agustincastro,irb@gmail.com>
2023-04-20 23:59:59 -0300

    1.11: unicafe, paso 6

--- part1/unicafe/src/SectionStatisties.jsx
+++ part1/unicafe/src/SectionStatisties.jsx
@@ -1,21 +1,32 @@
 
+const StatisticLine = ({ text, value }) => {
+    return (
+        <tr>
+            <td>{text}</td>
+            <td>{value}</td>
+        </tr>
+    )
+}
+
 const SectionStatisties = ({good, neutral, bad}) => {
     const all = good + neutral + bad;
-    let average = (good + bad *-1) / 3
-    let positive = good ? (good * 100) / all : 0;
+    let average = (good + (bad * -1)) / all
+    let positive = all ? (good * 100) / all : 0;
 
     return(
         <section>
-            <h2>statisties</h2>
+            <h2>statistics</h2>
             { all > 0 ?
-                <>
-                    <p>Good {good}</p>
-                    <p>Neutral {neutral}</p>
-                    <p>Bad {bad}</p>
-                    <p>all {all}</p>
-                    <p>average {average}</p>
-                    <p>positive {positive} %</p>
-                </> 
+                <table>
+                    <tbody>
+                        <StatisticLine text='Good' value={good}/>
+                        <StatisticLine text='Neutral' value={neutral}/>
+                        <StatisticLine text='Bad' value={bad}/>
+                        <StatisticLine text='All' value={all}/>
+                        <StatisticLine text='Average' value={average}/>
+                        <StatisticLine text='Positive' value={positive + '%'}/>
+                    </tbody>
+                </table> 
                 :
                 <p>No feedback give</p>
             }
