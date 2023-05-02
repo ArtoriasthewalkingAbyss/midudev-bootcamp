affd79ef3a161ed94e17a1613b37b2a66421cc76
artorias <agustincastro,irb@gmail.com>
2023-05-02 01:00:27 -0300

    1,14: anecdotes, paso 3

--- part1/anecdotes/src/App.jsx
+++ part1/anecdotes/src/App.jsx
@@ -1,29 +1,48 @@
-import { useState } from 'react'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
 
-function App({anecdotes}) {
-  const [selected, setSelected] = useState(0)
-  const [votes, setvotes] = useState(new Uint8Array(anecdotes.length))
-
-  const handleClickVotes = () => {
-    const copy = [...votes]
-    copy[selected] +=1
-    setvotes(copy)
-  }
-
-  function handleClickNext() {
-    let numberRamdom =  Math.floor(Math.random() * (anecdotes.length - 0) + 0)
-    setSelected(numberRamdom)
-  }
-
-  return (
-    <>
-      <h2>{anecdotes[selected]}</h2>
-      <p>Has {votes[selected]} votes</p>
-      <button onClick={handleClickVotes}>vote</button>
-      <button onClick={handleClickNext}>next anecdote</button>
-    </>
-  )
+function Anecdote({tilte, anecdote, voto}) {
+	return(
+		<section>
+			<h2>{tilte}</h2>
+			<p>{anecdote}</p>
+			<p>{voto}</p>
+		</section>
+	);
+}
+function Button({callback, text}) {
+	return (
+		<button onClick={callback}>{text}</button>
+	);
 }
 
-export default App
+function App({anecdotes}) {
+
+	const [selected, setSelected] = useState(0);
+	const [votes, setvotes] = useState(new Uint8Array(anecdotes.length));
+	const maxVotes = Math.max( ...votes );
+	const mostVotedIndex = votes.indexOf(maxVotes);
+	console.log(mostVotedIndex);
+
+	const handleClickVotes = () => {
+		const copy = [...votes];
+		copy[selected] +=1;
+		setvotes(copy);
+	};
+
+	function handleNextClick() {
+		let numberRamdom =  Math.floor(Math.random() * (anecdotes.length - 0) + 0);
+		setSelected(numberRamdom);
+	}
+
+	return (
+		<>
+			<Anecdote tilte={"Anecdote of the day"} anecdote={anecdotes[selected]} voto={`Has ${votes[selected]} votes`}/>
+			<Button callback={handleClickVotes} text={"vote"}/>
+			<Button callback={handleNextClick} text={"next anecdote"}/>
+			<Anecdote tilte={"Anecdote with most votes"} anecdote={`Has ${votes[mostVotedIndex]} votes`} voto={anecdotes[mostVotedIndex]}/>
+		</>
+	);
+}
+
+export default App;
