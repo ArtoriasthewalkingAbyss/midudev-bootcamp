b2d91cf98faf27cf53c4204bbf775d486ec4e478
artorias <agustincastro,irb@gmail.com>
2023-05-24 01:13:41 -0300

    2.7: La guía telefónica Paso2

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,14 +1,39 @@
 import { useState } from "react";
 import "./App.css";
 
-const App = () => {
-	const [ persons, setPersons ] = useState([
-		{ 
+function Form({ handleChange, handleSubmit }) {
+	return (
+		<form onSubmit={handleSubmit}>
+			<div>
+				<label htmlFor="name-input">Name:</label>
+				<input id="name-input" type="text" onChange={handleChange} />
+			</div>
+			<div>
+				<button type="submit">add</button>
+			</div>
+		</form>
+	);
+}
+
+function PersonsList({ persons }) {
+	return (
+		<section>
+			<h2>Numbers</h2>
+			{persons.map((value) => {
+				return <h3 key={value.id}>{value.name}</h3>;
+			})}
+		</section>
+	);
+}
+
+function App() {
+	const [persons, setPersons] = useState([
+		{
 			id: 1,
-			name: "Arto Hellas" 
-		}
-	]); 
-	const [ newName, setNewName ] = useState("");
+			name: "Arto Hellas",
+		},
+	]);
+	const [newName, setNewName] = useState("");
 
 	const handleChange = (event) => {
 		setNewName(event.target.value);
@@ -17,35 +42,26 @@ const App = () => {
 	const handleSubmit = (event) => {
 		event.preventDefault();
 
+		if (persons.some((value) => value.name === newName)) {
+			alert(newName + "ya esta agregado a su agenda");
+			return;
+		}
+
 		const personToAddToState = {
 			id: persons.length + 1,
-			name: newName
+			name: newName,
 		};
 
 		setPersons(persons.concat(personToAddToState));
-		
 	};
 
 	return (
 		<main>
 			<h2>Phonebook</h2>
-			<form onSubmit={handleSubmit}>
-				<div>
-                    name: <input onChange={handleChange}/>
-				</div>
-				<div>
-					<button type="submit">add</button>
-				</div>
-			</form>
-
-			<section>
-				<h2>Numbers</h2>
-				{persons.map(value => {
-					return <h3 key={value.id}>{value.name}</h3>;
-				})}
-			</section>
+			<Form handleChange={handleChange} handleSubmit={handleSubmit} />
+			<PersonsList persons={persons} />
 		</main>
 	);
-};
+}
 
 export default App;
