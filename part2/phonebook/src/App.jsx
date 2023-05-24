a88ee96045ffcb4479a0badc7e89beaedb555ccd
artorias <agustincastro,irb@gmail.com>
2023-05-24 16:49:38 -0300

    2.8: La guía telefónica Paso3

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -5,8 +5,12 @@ function Form({ handleChange, handleSubmit }) {
 	return (
 		<form onSubmit={handleSubmit}>
 			<div>
-				<label htmlFor="name-input">Name:</label>
-				<input id="name-input" type="text" onChange={handleChange} />
+				<label htmlFor="name-input">Name: </label>
+				<input id="name-input" type="text" onChange={handleChange} name="name"/>
+			</div>
+			<div>
+				<label htmlFor="number-input">Number: </label>
+				<input id="number-input" type="number" onChange={handleChange} name="number"/>
 			</div>
 			<div>
 				<button type="submit">add</button>
@@ -20,7 +24,7 @@ function PersonsList({ persons }) {
 		<section>
 			<h2>Numbers</h2>
 			{persons.map((value) => {
-				return <h3 key={value.id}>{value.name}</h3>;
+				return <h3 key={value.id}>{value.name}, {value.number} </h3>;
 			})}
 		</section>
 	);
@@ -31,18 +35,26 @@ function App() {
 		{
 			id: 1,
 			name: "Arto Hellas",
+			number: 454687461687
 		},
 	]);
 	const [newName, setNewName] = useState("");
+	const [newNumber, setNewNumber] = useState("");
 
 	const handleChange = (event) => {
-		setNewName(event.target.value);
+		console.log(event);
+		const { name, value } = event.target;
+		if (name === "name") {
+			setNewName(value);
+		} else if (name === "number") {
+			setNewNumber(value);
+		}
 	};
 
 	const handleSubmit = (event) => {
 		event.preventDefault();
 
-		if (persons.some((value) => value.name === newName)) {
+		if (persons.some((value) => value.name === newName || value.number === newNumber)) {
 			alert(newName + "ya esta agregado a su agenda");
 			return;
 		}
@@ -50,6 +62,7 @@ function App() {
 		const personToAddToState = {
 			id: persons.length + 1,
 			name: newName,
+			number: newNumber,
 		};
 
 		setPersons(persons.concat(personToAddToState));
