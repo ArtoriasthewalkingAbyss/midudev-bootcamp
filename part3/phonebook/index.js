a30109dcc606f1f6038ee77ec9ef6b2d2b709079
ArtoriasthewalkingAbyss <agustincastro.irb@gmail.com>
2023-07-25 12:13:15 -0300

    3.3: backend de la agenda telefónica, paso 3

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -35,6 +35,16 @@ app.get("/info", (request, response) => {
   response.send(`<p>La agenda tiene información de ${cabtidadContactos} personas</p> <p>${Date()}</p>`)
 })
 
+app.get('/api/persons/:id', (request, response) => {
+  const id = Number(request.params.id)
+  const person = persons.find(person => person.id === id)
+
+  if (person) {
+    response.json(person)
+  } else {
+    response.status(404).end()
+  }
+})
 
 const PORT = 3001
 app.listen(PORT, () => {
