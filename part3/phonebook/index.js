2c96e3bcbe9b04706e9d29dfa8ed5f900b2fec0c
ArtoriasthewalkingAbyss <agustincastro.irb@gmail.com>
2023-08-28 18:26:36 -0300

    3.15: Base de datos de la agenda telefónica, paso 3

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -72,10 +72,13 @@ app.get("/api/persons/:id", (request, response) => {
 })
 
 app.delete("/api/persons/:id", (request, response) => {
-  const id = Number(request.params.id)
-  persons = persons.filter(person => person.id !== id)
-
-  response.status(204).end()
+  const {id} = request.params;
+  
+  Contact.findByIdAndRemove(id)
+    .then(result => {
+      response.status(204).end()
+    })
+    .catch(error => console.log(error))
 })
 
 app.post("/api/persons", (request, response) => {
